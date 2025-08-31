287c207e455d0e7d3ef5f07daf9dd8cf9ff63395
shafrinahamed097 <shafrinahamedsa@gmail.com>
2025-08-31 14:15:11 +0600

    Add everything

--- index.js
+++ index.js
@@ -34,13 +34,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    const coffeeCollection = client.db("coffeeDBPracice").collection("coffee");
+    const coffeeCollection =  client.db("coffeeDBPractoce").collection('coffeePractice');
 
-    app.post("/addCoffee", async (req, res) => {
+    // send a data
+    app.post("/coffee", async(req,res)=>{
       const newCoffee = req.body;
-      console.log(newCoffee);
       const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result);
+    });
+
+    // Get A Data
+    app.get("/coffee", async(req,res)=>{
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+
     });
 
     await client.db("admin").command({ ping: 1 });
